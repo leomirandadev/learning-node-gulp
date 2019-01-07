7da4202636ba0b983c8f7b11dc16b53134e8ca5e
LeoMiranda <leoamiranda2@gmail.com>
2019-01-06 23:22:45 -0200

    ajuste no gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,50 +6,38 @@ var rename = require('gulp-rename');
 var jquery = require('gulp-jquery');
 var minify_css = require('gulp-minify-css');
 var imagemin = require('gulp-imagemin');
+// database
+var data = {
+    js:{ src:'./src/js/*.js', dist:'./js', new_name:'main.js' },
+    css:{ src:'./src/css/*.css', dist:'./css', dist:'main.css' },
+    img:{ src:'./src/img/*', dist:'./img' },
+    jquery:{ new_name:'jquery.js' }
+}
 
-// source path
-var js_src = './src/js/*.js';
-var css_src = './src/css/*.css';
-var img_src = './src/img/*';
-
-// dist path
-var js_dist = './js';
-var css_dist = './css';
-var img_dist = './img/';
-
-var js_dist_name = 'main.js';
-var js_dist_jquery_name = 'jquery.js';
-var css_dist_name = 'main.css';
-
-// minify and concat scripts
+// exec all tasks
+gulp.task('minifyall', gulp.series('css', 'jquery', 'scripts', 'img') );
+// tasks
 gulp.task('scripts', function(){
-    return gulp.src(js_src)
-        .pipe(plumber())
-        .pipe(uglify())
-        .pipe(concat(js_dist_name))
-        .pipe(gulp.dest(js_dist));
+    return gulp.src(data.js.src)
+        .pipe( plumber() ).pipe( uglify() )
+        .pipe( concat(data.js.new_name) )
+        .pipe( gulp.dest(data.js.dist) );
 });
 gulp.task('css', function(){
-    return gulp.src(css_src)
-        .pipe(minify_css({keepSpecialComments: 1}))
-        .pipe(concat(css_dist_name))
-        .pipe(gulp.dest(css_dist));
+    return gulp.src(data.css.src)
+        .pipe( minify_css({keepSpecialComments: 1}) )
+        .pipe( concat(data.css.new_name) )
+        .pipe( gulp.dest(data.css.dist) );
 });
 gulp.task('img', function(){
-    return gulp.src(img_src)
-        .pipe(imagemin({progressive: true}))
-        .pipe(gulp.dest(img_dist));
+    return gulp.src(data.img.src)
+        .pipe( imagemin({progressive: true}) )
+        .pipe( gulp.dest(data.img.dist) );
 });
 gulp.task('jquery', function () {
-    return jquery.src({
-        release: 2, //jQuery 2
-        flags: ['-deprecated']
-    })
-    .pipe(plumber())
-    .pipe(uglify())
-    .pipe(concat(js_dist_jquery_name))
-    .pipe(gulp.dest(js_dist));
+    return jquery.src({ release:2, flags:['-deprecated'] })
+        .pipe( plumber() )
+        .pipe( uglify() )
+        .pipe( concat(data.jquery.new_name) )
+        .pipe( gulp.dest(data.js.dist) );
 });
-
-// executa todas as tasks
-gulp.task('minifyall', gulp.series('css', 'jquery', 'scripts', 'img') );
