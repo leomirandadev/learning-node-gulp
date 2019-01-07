ac9cbc922c69f696622ed8dca7aa6dfb189b7a55
LeoMiranda <leoamiranda2@gmail.com>
2019-01-06 23:36:30 -0200

    ajuste gulp 2

--- gulpfile.js
+++ gulpfile.js
@@ -6,38 +6,33 @@ var rename = require('gulp-rename');
 var jquery = require('gulp-jquery');
 var minify_css = require('gulp-minify-css');
 var imagemin = require('gulp-imagemin');
-// database
+// database ==============================================================
 var data = {
     js:{ src:'./src/js/*.js', dist:'./js', new_name:'main.js' },
     css:{ src:'./src/css/*.css', dist:'./css', dist:'main.css' },
     img:{ src:'./src/img/*', dist:'./img' },
     jquery:{ new_name:'jquery.js' }
 }
-
-// exec all tasks
+// exec all tasks ========================================================
 gulp.task('minifyall', gulp.series('css', 'jquery', 'scripts', 'img') );
-// tasks
+// tasks =================================================================
 gulp.task('scripts', function(){
     return gulp.src(data.js.src)
-        .pipe( plumber() ).pipe( uglify() )
-        .pipe( concat(data.js.new_name) )
-        .pipe( gulp.dest(data.js.dist) );
+    .pipe( plumber() ).pipe( uglify() )
+    .pipe( concat(data.js.new_name) ).pipe( gulp.dest(data.js.dist) );
 });
 gulp.task('css', function(){
     return gulp.src(data.css.src)
-        .pipe( minify_css({keepSpecialComments: 1}) )
-        .pipe( concat(data.css.new_name) )
-        .pipe( gulp.dest(data.css.dist) );
+    .pipe( minify_css({keepSpecialComments: 1}) )
+    .pipe( concat(data.css.new_name) ).pipe( gulp.dest(data.css.dist) );
 });
 gulp.task('img', function(){
     return gulp.src(data.img.src)
-        .pipe( imagemin({progressive: true}) )
-        .pipe( gulp.dest(data.img.dist) );
+    .pipe( imagemin({progressive: true}) )
+    .pipe( gulp.dest(data.img.dist) );
 });
 gulp.task('jquery', function () {
     return jquery.src({ release:2, flags:['-deprecated'] })
-        .pipe( plumber() )
-        .pipe( uglify() )
-        .pipe( concat(data.jquery.new_name) )
-        .pipe( gulp.dest(data.js.dist) );
+    .pipe( plumber() ).pipe( uglify() )
+    .pipe( concat(data.jquery.new_name) ).pipe( gulp.dest(data.js.dist) );
 });
